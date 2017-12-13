7fbad3ecf210f98376225836f193dc5d09d1838d
Shams Ul Azeem <shamsazeem20@gmail.com>
2017-12-13 16:35:58 +0500

    General request classes structure

--- endpoint_clients/src/main/java/ai/skymind/skil/examples/endpoints/Transform.java
+++ endpoint_clients/src/main/java/ai/skymind/skil/examples/endpoints/Transform.java
@@ -1,4 +1,18 @@
 package ai.skymind.skil.examples.endpoints;
 
 public class Transform {
+
+    private String host;
+    private String port;
+
+    public Transform() {
+        this.host = "localhost";
+        this.port = "9008";
+    }
+
+    public Transform(String host, String port) {
+        this.host = host;
+        this.port = port;
+    }
+
 }
